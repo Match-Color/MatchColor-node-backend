0b53f8d70b1cf94799c972234395623237024780
soyeonvv <thdusdlvv@naver.com>
2022-11-05 20:08:41 +0900

    [ADD] login, signup router

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ router.use("/main", require("./mainRouter"));
 router.use("/detail", require("./detailRouter"));
 router.use("/upload", require("./uploadRouter"));
 router.use("/mypage", require("./mypageRouter"));
+router.use("/login", require("./loginRouter"));
 
 module.exports = router;
